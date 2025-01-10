557096a56fe50eb8f2cf5b55122e98a2dd6340a2
JieHengYu <70485082+JieHengYu@users.noreply.github.com>
2025-01-09 18:57:26 -0800

    Add files via upload

---
+++ PostgreSQL/Chapter_18/Exercises.sql
@@ -0,0 +1,11 @@
+SELECT * 
+FROM tl_2019_35049_linearwater_shp
+LIMIT 3;
+
+SELECT *
+FROM tl_2019_35049_roads_shp
+LIMIT 3;
+
+DROP TABLE tl_2019_35049_linearwater_shp;
+
+DROP TABLE tl_2019_35049_roads_shp;
